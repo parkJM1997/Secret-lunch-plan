c0a6c91fe855dfd00771e0ad2b78b5e00ea4ea9e
parkJM1997 <boos56@naver.com>
2021-08-18 12:57:08 +0900

    Start

--- cookiedraw/src/App.js
+++ cookiedraw/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cookie-Run-Draw
         </a>
       </header>
     </div>
