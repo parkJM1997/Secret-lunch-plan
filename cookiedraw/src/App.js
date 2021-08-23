963526b1f0f15fc5d76d7ef3fd273ded8cefdac3
parkJM1997 <boos56@naver.com>
2021-08-23 12:52:50 +0900

    random

--- cookiedraw/src/App.js
+++ cookiedraw/src/App.js
@@ -1,7 +1,26 @@
+import React, {useState} from 'react';
+
 function App() {
+  const [cookie, setCookie] = useState('');
+  
+
+  function DrawStart(e) {
+    if (e.currentTarget.id == "1Draw") {
+      setCookie(Math.floor (Math.random() * 10) + 1);
+    } else if (e.currentTarget.id == "10Draw") {
+      setCookie(cookie + " " + Math.floor (Math.random() * 10 + 1));
+    }
+  }
+
   return (
     <div className="App">
-      쿠키뽑기 test
+      쿠키뽑기 test 
+      <>
+      <button id="1Draw" onClick={DrawStart}> 뽑기 </button> <button id="10Draw" onClick={DrawStart}> 10회뽑기 </button>
+      </>
+      <div>
+        {cookie}
+      </div>
     </div>
   );
 }
